f25bfc0001459e6ccf426df6e508224000c6414c
Claudio <cl.silveira@gmail.com>
2025-01-06 23:23:32 -0300

    limpeza

--- src/main/java/cms/security/SecurityConfig.java
+++ src/main/java/cms/security/SecurityConfig.java
@@ -41,46 +41,10 @@ public class SecurityConfig {
 				    .logoutSuccessUrl("/")
 					.permitAll()
 			);
-		
-// Versao deprectada
-//		http
-//		.authorizeHttpRequests()
-//		.requestMatchers("/","/legendas","/bootstrap/**","/css/**","/fonts/**","/footable/**","/image/**","/jquery/**","/js/**")
-//		.permitAll()
-//		.requestMatchers("/legendas/login/*").hasAnyRole("admin","worker","login")
-//		.requestMatchers("/legendas/worker/*").hasAnyRole("admin","worker")
-//		.requestMatchers("/legendas/admin/*").hasRole("admin")
-//		.anyRequest().authenticated()
-//		.and()
-//		.formLogin().loginPage("/login").permitAll()
-//		.and()
-//		.logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/legendas").permitAll();
-
 		return http.build();
 	}
 
-	//@Bean
-	public UserDetailsService userDetailsService(PasswordEncoder enc) {
 
-		//precisa adicionar o prefixo "ROLE_" nos nomes das roles.
-		//varios metodos da api http e spring adicionam isso ao comparar nomes de roles.
-		//vc so precisa usar o prefixo ao cadastrar as roles com o usuario.
-		
-		UserDetails user3 =
-				 User.withUsername("worker@gmail.com")
-					.password(enc.encode("123"))
-					.roles("worker","login") //roles() : adiciona o prefixo "ROLE_" automaticamente
-					.build();
-		
-		UserDetails user4 =
-				 User.withUsername("cl.silveira@gmail.com")
-					.password(enc.encode("123"))
-					.authorities("ROLE_worker","ROLE_login") //authorities() : NAO adiciona o prefixo "ROLE_"
-					.build();
-		
-
-		return new InMemoryUserDetailsManager(user3,user4);
-	}
 	
 	@Bean
 	PasswordEncoder passwordEncoder() {
